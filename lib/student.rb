a3ce8bb444c727d3c02aa784035712161a88caf6
davidnene <davienesh4@gmail.com>
2022-08-21 05:23:03 +0300

    commit tree aee4332162ad6019313349e4e30f34b8210ce306

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user'
+class Student < User
+    
+    
+    
+    def initialize
+        @knowledge = []
+    end
 
+    def learn(value)
+        @knowledge << value
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
